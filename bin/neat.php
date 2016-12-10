ea2522fe29be3750e250a7e2103db333bd7113b7
jungle <jungle@ranqiangjun.com>
2016-12-11 00:23:43 +0800

    Fix autoload

--- bin/neat.php
+++ bin/neat.php
@@ -1,7 +1,17 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
+$root = __DIR__ . '/../';
+if (file_exists($root . 'vendor/autoload.php')) {
+  include_once $root . 'vendor/autoload.php';
+}
+elseif (file_exists($root . '../../autoload.php')) {
+  include_once $root . '../../autoload.php';
+}
+else {
+  echo 'Something goes wrong with your archive' . PHP_EOL .
+    'Try downloading again' . PHP_EOL;
+  exit(1);
+}
 
 use Symfony\Component\Console\Application;
 use Neat\Command\AboutCommand;
